7cfcb466cac0f520c2df2f8a4688719a221ce521
ephrem <ephremmamo555@gmail.com>
2024-10-01 00:47:07 +0300

    finished hero section

--- fluidai/src/components/Header.tsx
+++ fluidai/src/components/Header.tsx
@@ -1,16 +1,20 @@
 import { Link } from 'react-router-dom';
 import './Header.css';
+import humburger from '../assets/Frame.png';
 export default function Header() {
   return (
-    <nav className="nav_bar">
-      <div>
-        <Link to="/what-we-believe">What we believe</Link>
-        <Link to="/our-features">Our features</Link>
-      </div>
-      <div>
-        <Link to="/our-story">Our story</Link>
-        <Link to="/the-waitlist">The waitlist</Link>
-      </div>
-    </nav>
+    <>
+      <img className="humburger" src={humburger} alt="humburger" />
+      <nav className="nav_bar">
+        <div>
+          <Link to="/what-we-believe">What we believe</Link>
+          <Link to="/our-features">Our features</Link>
+        </div>
+        <div>
+          <Link to="/our-story">Our story</Link>
+          <Link to="/the-waitlist">The waitlist</Link>
+        </div>
+      </nav>
+    </>
   );
 }
