fd33b6553857cdfa0f0d9cde7fb12b58437f51b5
ephrem <ephremmamo555@gmail.com>
2024-10-01 13:50:07 +0300

    gradient colors added

--- fluidai/src/components/Us.tsx
+++ fluidai/src/components/Us.tsx
@@ -1,10 +1,18 @@
 import Button from './core/Button';
+import bg from '../assets/hero-bg.png';
 import './Us.css';
 export default function Us() {
+  const containerStyle = {
+    backgroundImage: `url(${bg})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  };
   return (
-    <div className="us">
-      <h1>Unlock Seamless Efficiency</h1>
-      <Button />
+    <div style={containerStyle} className="us_container">
+      <div className="us">
+        <h1>Unlock Seamless Efficiency</h1>
+        <Button />
+      </div>
     </div>
   );
 }
